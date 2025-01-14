67bb32714088456f2df0a980862eccbce7527663
GurgenProgrammer <gurgen.bayburdyan.10@gmail.com>
2025-01-14 17:49:40 +0400

    Initial Commit

---
+++ src/test/java/com/example/schooloperationsystem/SchoolOperationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schooloperationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolOperationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

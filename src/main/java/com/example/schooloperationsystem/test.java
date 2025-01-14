b455086529691e611df907da06e52c3054b39265
GurgenProgrammer <gurgen.bayburdyan.10@gmail.com>
2025-01-14 23:05:20 +0400

    .

---
+++ src/main/java/com/example/schooloperationsystem/test.java
@@ -0,0 +1,15 @@
+package com.example.schooloperationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gurgen Bayburdyan
+ */
+
+@SpringBootApplication
+public class test {
+    public static void main(String[] args) {
+        SpringApplication.run(test.class, args);
+    }
+}

c5cf18d591b6683f1fc318dc1c5df7634eaf2c52
GurgenProgrammer <gurgen.bayburdyan.10@gmail.com>
2025-01-16 20:22:37 +0400

    .

---
+++ src/main/java/com/example/schooloperationsystem/rest/dto/SchoolClassDto.java
@@ -0,0 +1,13 @@
+package com.example.schooloperationsystem.rest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SchoolClassDto {
+    private Character classLetter;
+    private Integer grade;
+}

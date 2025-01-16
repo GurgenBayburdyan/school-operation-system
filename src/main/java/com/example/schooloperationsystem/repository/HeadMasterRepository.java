19c7055547b33522792e4a31b5743c2fbbb91bae
GurgenProgrammer <gurgen.bayburdyan.10@gmail.com>
2025-01-16 21:39:42 +0400

    .

--- src/main/java/com/example/schooloperationsystem/repository/HeadMasterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.schooloperationsystem.repository;
-
-import com.example.schooloperationsystem.entity.HeadMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HeadMasterRepository extends JpaRepository<HeadMaster, Long> {
-}

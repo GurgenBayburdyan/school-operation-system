80e2912f7e8e536b4aa17ace9b692bbb1a81ba5e
Arman Gevorgyan <39789090+gevorgyanarman@users.noreply.github.com>
2025-01-24 10:49:56 +0400

    Update CreateStaffParams.java

--- src/main/java/com/example/schooloperationsystem/service/params/CreateStaffParams.java
+++ src/main/java/com/example/schooloperationsystem/service/params/CreateStaffParams.java
@@ -7,12 +7,15 @@ import java.time.LocalDateTime;
 
 @Setter
 @Getter
+    //todo please implement toString
 public class CreateStaffParams {
+    //todo lets make final
     private String firstName;
     private String lastName;
     private LocalDateTime dateOfBirth;
 
     public CreateStaffParams(String firstName, String lastName, LocalDateTime dateOfBirth) {
+        //todo please assert all fields one by one
         if (firstName == null || lastName == null || dateOfBirth == null) {
             throw new IllegalArgumentException();
         }

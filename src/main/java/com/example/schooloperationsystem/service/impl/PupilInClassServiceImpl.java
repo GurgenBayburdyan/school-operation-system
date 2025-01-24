830af03eaec5b5d3ae1bd4b55e6064777a82b33a
Arman Gevorgyan <39789090+gevorgyanarman@users.noreply.github.com>
2025-01-24 10:48:29 +0400

    Update PupilInClassServiceImpl.java

--- src/main/java/com/example/schooloperationsystem/service/impl/PupilInClassServiceImpl.java
+++ src/main/java/com/example/schooloperationsystem/service/impl/PupilInClassServiceImpl.java
@@ -55,6 +55,7 @@ class PupilInClassServiceImpl implements PupilInClassService {
 
     @Override
     @Transactional
+    //todo please validate pupil existence in controller, if not found return PUPIL_NOT_FOUND error. 
     public PupilInClass deletePupilById(Long id) {
         log.debug("Executing delete pupil in class by id, id-{}", id);
 

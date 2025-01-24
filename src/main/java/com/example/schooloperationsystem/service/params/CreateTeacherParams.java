eaa6dec1945a4b6a1d2f44170b40a9b99aa458b7
Arman Gevorgyan <39789090+gevorgyanarman@users.noreply.github.com>
2025-01-24 10:49:18 +0400

    Update CreateTeacherParams.java

--- src/main/java/com/example/schooloperationsystem/service/params/CreateTeacherParams.java
+++ src/main/java/com/example/schooloperationsystem/service/params/CreateTeacherParams.java
@@ -9,6 +9,7 @@ public class CreateTeacherParams {
     private Long staffId;
 
     public CreateTeacherParams(Long staffId) {
+        //todo Assert.notNull(staffId, "the staff id should not be null);
         if (staffId == null) {
             throw new IllegalArgumentException();
         }

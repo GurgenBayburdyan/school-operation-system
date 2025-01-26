a27bb4bdc23b89bca8bf98ec03b37c77a30228a2
Arman Gevorgyan <39789090+gevorgyanarman@users.noreply.github.com>
2025-01-26 15:07:11 +0400

    Update PupilInClassValidator.java

--- src/main/java/com/example/schooloperationsystem/rest/controller/validator/PupilInClassValidator.java
+++ src/main/java/com/example/schooloperationsystem/rest/controller/validator/PupilInClassValidator.java
@@ -5,6 +5,7 @@ import com.example.schooloperationsystem.rest.dto.response.ErrorType;
 
 import java.util.Optional;
 
+//todo must be a component
 public class PupilInClassValidator {
 
     public Optional<ErrorType> validateCreate(CreatePupilInClassRequestDto requestDto) {

6b0eec50aa33b0f63f6b54115dfe26cb21f6b816
Arman Gevorgyan <39789090+gevorgyanarman@users.noreply.github.com>
2025-03-16 09:54:39 +0400

    Update SchoolFacadeImpl.java

--- src/main/java/com/example/schooloperationsystem/rest/facade/impl/SchoolFacadeImpl.java
+++ src/main/java/com/example/schooloperationsystem/rest/facade/impl/SchoolFacadeImpl.java
@@ -6,7 +6,7 @@ import com.example.schooloperationsystem.entity.School;
 import com.example.schooloperationsystem.entity.Staff;
 import com.example.schooloperationsystem.entity.Teacher;
 import com.example.schooloperationsystem.mapper.SchoolMapper;
-import com.example.schooloperationsystem.rest.controller.validator.SchoolValidator;
+import com.example.schooloperationsystem.rest.controller.validator.SchoolValidator; //todo move validator to facade validator package
 import com.example.schooloperationsystem.rest.dto.request.CreateSchoolRequestDto;
 import com.example.schooloperationsystem.rest.dto.response.ErrorType;
 import com.example.schooloperationsystem.rest.dto.response.SchoolDetailsDto;
@@ -22,9 +22,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Service //todo change to @Component
 @Slf4j
 @AllArgsConstructor
+    //todo make package private
 public class SchoolFacadeImpl implements SchoolFacade {
 
     private final SchoolService schoolService;
@@ -33,22 +34,24 @@ public class SchoolFacadeImpl implements SchoolFacade {
 
     @Override
     public SchoolDetailsDto create(CreateSchoolRequestDto requestDto) {
+        //todo chaneg to debug log level
         log.info("Executing create school for the provided request to - {}", requestDto);
 
         Optional<ErrorType> optionalErrorType = schoolValidator.validateCreate(requestDto);
 
         if (optionalErrorType.isPresent()) {
-            SchoolDetailsDto schoolClassDetailsDto = new SchoolDetailsDto();
+            SchoolDetailsDto schoolClassDetailsDto = new SchoolDetailsDto(); //todo create a constructor with errortype
             schoolClassDetailsDto.setErrorType(optionalErrorType.get());
             log.info("Executing create school failed, error-{}", optionalErrorType.get());
             return schoolClassDetailsDto;
-        } else {
+        } else { //todo no need to write else, becuase you have return statemenet above
             CreateSchoolParams params = new CreateSchoolParams(
                     requestDto.getNumber(),
                     requestDto.getNamedAfter(),
                     requestDto.getAddress(),
                     requestDto.getPhotoUrl()
             );
+            //todo please add final eywords wherever possible. e.g. final School scoole = schoolService.....
             School response = schoolService.create(params);
             SchoolDetailsDto schoolDetailsDto = schoolMapper.map(response);
 
@@ -61,7 +64,7 @@ public class SchoolFacadeImpl implements SchoolFacade {
     public List<SchoolDetailsDto> get() {
         log.info("Executing get all schools rest API");
 
-        List<School> response = schoolService.get();
+        List<School> response = schoolService.get(); //todo rename response to schoolList
         List<SchoolDetailsDto> schoolDetailsDtos = schoolMapper.mapList(response);
 
         log.info("Successfully executed get schools rest API, response - {}", schoolDetailsDtos);
@@ -72,7 +75,8 @@ public class SchoolFacadeImpl implements SchoolFacade {
     public SchoolDetailsDto getById(Long id) {
         log.info("Executing get school by id-{}", id);
 
-        School response = schoolService.getById(id);
+        School response = schoolService.getById(id); //todo service getById must return no-nullabel value. if not found it must throw an exception
+        //todo better to use findById that returns Optional of school
 
         if (response == null) {
             SchoolDetailsDto schoolDetailsDto = new SchoolDetailsDto();

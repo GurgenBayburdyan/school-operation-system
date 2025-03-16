42ceb4c1127db29c653af18b704e086cdfbe4d5d
Arman Gevorgyan <39789090+gevorgyanarman@users.noreply.github.com>
2025-03-16 09:50:18 +0400

    Update SchoolController.java

--- src/main/java/com/example/schooloperationsystem/rest/controller/SchoolController.java
+++ src/main/java/com/example/schooloperationsystem/rest/controller/SchoolController.java
@@ -18,17 +18,21 @@ public class SchoolController {
 
     private final SchoolFacade schoolFacade;
 
+    //todo add info logs
     @GetMapping
+    //todo rename to getAll()
     public ResponseEntity<List<SchoolDetailsDto>> getClasses() {
         return ResponseEntity.ok(schoolFacade.get());
     }
 
     @PostMapping
+    //todo rename to create()
     public ResponseEntity<SchoolDetailsDto> createClass(@RequestBody CreateSchoolRequestDto requestDto) {
         return ResponseEntity.ok(schoolFacade.create(requestDto));
     }
 
     @GetMapping("/{id}")
+    //todo rename to getById
     public ResponseEntity<SchoolDetailsDto> getClassById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(schoolFacade.getById(id));
     }
